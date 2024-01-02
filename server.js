487bb927b07f752dc3c360fac2fd251638e05545
Michael Parque <michael.t.parque@outlook.com>
2024-01-02 13:36:17 -0500

    Add a basic /health route

--- server.js
+++ server.js
@@ -34,7 +34,42 @@ wss.on('connection', (ws) => {
 // Serve static files (HTML, CSS, JS)
 app.use(express.static('public'));
 
+app.get('/health', (req, res, next) => {
+    res.type('application/json');
+    try {
+        res.send(JSON.stringify(healthCheck()));
+    } catch (err) {
+        next(err);
+    }
+});
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Internal server error.');
+})
+
 server.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
 
+function healthCheck() {
+    clientCounts = {
+        "Closed": 0,
+        "Open": 0,
+        "Closing": 0,
+        "Connecting": 0,
+        "Unknown": 0
+    };
+    wss.clients.forEach((client) => {
+        const state =
+            client.readyState === WebSocket.CLOSED ? "Closed" :
+            client.readyState === WebSocket.OPEN ? "Open" :
+            client.readyState === WebSocket.CLOSING ? "Closing" :
+            client.readyState === WebSocket.CONNECTING ? "Connecting" :
+            "Unknown" ;
+        clientCounts[state] = (clientCounts[state] || 0) + 1
+    });
+    return {
+        clientCounts
+    };
+}
